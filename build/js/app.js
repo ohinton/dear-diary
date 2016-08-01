eb063376b1b45308bc35142f102dbd0002ad432c
Meaghan Jones and Olivia Hinton <meaghan@>
2016-08-01 16:33:10 -0700

    add frontend

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Diary(title, entry){
   this.title = title;
   this.entry = entry;
@@ -49,3 +50,21 @@ Diary.prototype.getTeaser = function() {
 
 
 exports.diaryModule = Diary;
+
+},{}],2:[function(require,module,exports){
+var Diary = require('./../js/diary.js').diaryModule;
+
+
+$(document).ready(function() {
+  $("form#diary").submit(function() {
+    event.preventDefault();
+  var inputtedTitle = $("input#title").val();
+  var inputtedEntry = $("input#entry").val();
+  var newDiary = new Diary(inputtedTitle, inputtedEntry);
+
+  $("#printed-title").text(newDiary.title);
+  $("#printed-entry").text(newDiary.entry);
+  });
+});
+
+},{"./../js/diary.js":1}]},{},[2]);
